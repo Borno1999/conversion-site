3210b3a9fbdd36f53575067d975776c5005b4a19
Borno1999 <84654560+Borno1999@users.noreply.github.com>
2021-06-23 10:55:41 +0600

    Add files via upload

---
+++ page2.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<body>
+<fieldset>
+<h1 style="color:red;text-align:left">Page 2 [Conversion Site]</h1>
+<h2 style="color:red;text-align:left">Conversion Site</h2>
+
+
+<h5> <a href="page1.php">Home</a></h5>
+
+<h5> <a href="page2.php">Conversion Rate</a></h5>
+
+
+ <h5> <a href="page3.php">History</a></h5>
+
+ <h7 style="color:red;text-align:left">Conversion Site</h7><br><br>
+
+ <input type="text" value="feet to inch "> 
+ 
+
+<input type="text" value="1">
+ 
+
+ 
+<input type="text" value="12">
+ 
+ <br/>
+ 
+</body>
+</fieldset>
+</html>
+
+<?php
+
+
+?>   
+
+
---
+++ page3.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<body>
+<fieldset>
+<h1 style="color:red;text-align:left">Page 3 [History]</h1>
+<h2 style="color:red;text-align:left">Conversion Site</h2>
+
+
+<h5> <a href="page1.php">1. Home</a></h5>
+
+<h5> <a href="page2.php">2. Conversion Rate</a></h5>
+
+
+ <h5> <a href="page3.php">3. History</a></h5>
+
+ <h7 style="color:red;text-align:left">Converter History</h7><br>
+ 
+ 1. feet to inch  <input type="text" value="3"> <input type="text" value="36">
+ 
+ 2. inch to feet  <input type="text" value="1"> <input type="text" value="12">
+ 
+ 
+ 
+
+</body>
+</fieldset>
+</html>
+
+<?php
+
+
+?>   
+
+
