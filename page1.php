15543d5017f6baefcccbd09e30c2e3b7b2f5c8cd
Borno1999 <farabial54@gmail.com>
2021-06-23 10:52:43 +0600

    first commit

---
+++ page1.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<body>
+<fieldset>
+<h1 style="color:red;text-align:left">Page 1 [Home]</h1>
+<h2 style="color:red;text-align:left">Conversion Site</h2>
+
+
+<h5> <a href="page1.php">1. Home</a></h5>
+
+<h5> <a href="page2.php">2. Conversion Rate</a></h5>
+
+
+ <h5> <a href="page3.php">3. History</a></h5>
+
+ <h7 style="color:red;text-align:left">Converter</h7><br>
+ <fieldset>
+ feet to inch 
+ </fieldset>
+ <br><br>
+ 
+ Value:<input type="text" value="3"><br><br>
+
+<br><br><br>
+ 
+Result:<input type="text" value="36">
+ 
+ <br/><br>
+ <input type="Submit" value="Submit">
+ 
+</body>
+</fieldset>
+</html>
+
+<?php
+
+
+?>   
+
+
